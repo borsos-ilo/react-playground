4527fcad2a93f6bead3dea1caba8b653802ba119
Ilona Kedracka <ilona.kedracka@gmail.com>
2022-11-18 20:55:29 +0100

    experiments with state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 function Button() {
-    const [inputText, setInputText] = useState("Default text");
+    const [inputText, setInputText] = useState("Default text"); // first one is variable, second is actually a function that changes the value of the first one 
+    // and initially I can provide some first-time value to it 
     function handleChange(e) {
-        setInputText(e.target.value)
+        setInputText(e.target.value) // where does .target.value come from?
+        // The target event property returns the element that triggered the event.
+
     };
 
     return (
         <>
-            <input value={inputText} onChange={handleChange}/>
+            <input value={inputText} onChange={handleChange}/> 
+            {/*Why not handleChange(inputText)???*/}
             <p>You typed: {inputText}</p>
-            <button onClick={() => setInputText("Default text")}>Reset</button>
+            <button onClick={() => setInputText("You did a reset!")}>Reset</button> 
         </>
     )
 }
