9d9bb59289db13d18b983546a3482270dd75cba7
Ilona Kedracka <ilona.kedracka@gmail.com>
2022-11-18 21:10:27 +0100

    committing changes to revert

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,17 @@
 function Main(){
     return(
         <>
-        <h2>They say it takes 10,000 hours to master a skill.</h2>
-        <h2>And I like challenges.</h2>
+        <div>
+            <h1>They say it takes 10,000 hours to master a skill.</h1>
+            <h2>But I don't think I need to master a skill.</h2>
+            <h3>In fact, I'm pretty sure that if I only spent 1000, 500 or 100 hours on certain skills, my life would improve significantly.</h3>
+            <p>So that's what I'm doing here - setting myself up for a challenge, tracking time spent on various activities and evaluating how does it change my life.</p>
+        </div>
+        <div>
+            <h1>
+                Welcome to my world - it's good to have you here.
+            </h1>
+        </div>
         </>
     );
 }
